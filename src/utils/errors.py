c344e30f472d0d4c1af78f3c67072c9931fe6006
Lukinhas <143443695+ThatLukinhasGuy@users.noreply.github.com>
2024-05-20 15:03:39 -0300

    Update errors.py

--- src/utils/errors.py
+++ src/utils/errors.py
@@ -5,22 +5,27 @@ from ..responses import PrettyJSONResponse
 from ..exceptions import BaseError, InvalidRequestException, InvalidResponseException
 
 def configure_error_handlers():
-    """Sets up error handlers to return error messages on certain cases"""
+    """Sets up all error handlers"""
 
     def status_404_handler(request: Request, _: Exception) -> PrettyJSONResponse:
+        """Not found page error handler"""
         return InvalidRequestException(f"Invalid URL ({request.method} {request.url.path})", status=404).to_response()
 
     def status_405_handler(request: Request, _: Exception) -> PrettyJSONResponse:
+        """Method not allowed error handler"""
         return InvalidRequestException(f"Invalid Method ({request.method} {request.url.path})", status=405).to_response()
 
     def exception_handler(_: Request, exc: Exception) -> PrettyJSONResponse:
+        """Generic error handler"""
         traceback.print_exc()
         return BaseError(f"An unexpected error has occurred: {exc}", status=500).to_response()
     
     def value_error_handler(_: Request, exc: Exception) -> PrettyJSONResponse:
+        """Validation error handler"""
         return InvalidRequestException(f"Invalid value: {exc}", status=400).to_response()
     
     def http_exception_handler(_: Request, exc: HTTPException) -> PrettyJSONResponse:
+        """HTTP exception handler"""
         return InvalidRequestException(exc.detail, status=exc.status_code).to_response()
 
     return {

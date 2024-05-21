12c9a2121a234023f8a841291f285783c005fd55
Lukinhas <143443695+ThatLukinhasGuy@users.noreply.github.com>
2024-05-21 19:53:04 -0300

    Simplify type hints

--- src/database/keys.py
+++ src/database/keys.py
@@ -1,5 +1,5 @@
 import ujson
-from typing import List, Union
+from typing import List, Optional
 from pymongo import MongoClient
 from asgiref.sync import sync_to_async
 
@@ -15,7 +15,7 @@ class KeyManager:
     db = client["db"]["keys"]
 
     @classmethod
-    async def get_keys(cls, key_type: str) -> List[Union[str, None]]:
+    async def get_keys(cls, key_type: str) -> List[Optional[str]]:
         """Returns a list of keys for a given key type"""
         keys = await sync_to_async(cls.db.find_one)({"name": key_type})
         return keys.get("keys") if keys else []
--- src/database/users.py
+++ src/database/users.py
@@ -1,6 +1,6 @@
 import ujson
 from pymongo import MongoClient
-from typing import Literal, Union, Dict
+from typing import Literal, Union, Dict, Optional
 from asgiref.sync import sync_to_async
 from ..utils import gen_random_string
 
@@ -29,19 +29,19 @@ class UserManager:
         return
 
     @classmethod
-    async def get_user_by_id(cls, user_id: int) -> Union[Dict[str, Union[str, bool]], None]:
+    async def get_user_by_id(cls, user_id: int) -> Optional[Dict[str, Union[str, bool]]]:
         """Returns an user by its ID"""
         user = await sync_to_async(cls.db.find_one)({"id": str(user_id)})
         return user.get("key") if user else None
 
     @classmethod
-    async def check_key(cls, key: str) -> Union[dict, None]:
+    async def check_key(cls, key: str) -> Optional[dict]:
         """Checks if a key exists in the database"""
         user = await sync_to_async(cls.db.find_one)({"key": key})
         return user
     
     @classmethod
-    async def get_property(cls, key: str, property: Literal["premium", "banned"]) -> Union[bool, None]:
+    async def get_property(cls, key: str, property: Literal["premium", "banned"]) -> Optional[bool]:
         """Gets an user's property (premium or banned)"""
         user = await sync_to_async(cls.db.find_one)({"key": key})
         return user.get(property) if user else None

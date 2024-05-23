a0243d6b7314127f53d3f3ffad9e2feb9352ca29
Lukinhas <143443695+ThatLukinhasGuy@users.noreply.github.com>
2024-05-23 14:53:09 -0300

    piss even more

--- src/typings/admin.py
+++ src/typings/admin.py
@@ -1,8 +1,7 @@
 from typing import Optional
-from dataclasses import dataclass
+from pydantic import BaseModel, field_validator
 
-@dataclass
-class AdminBody:
+class AdminBody(BaseModel):
     """
     The default body of admin requests
     """
@@ -11,3 +10,14 @@ class AdminBody:
     action: str
     status: Optional[bool] = None
     property: Optional[str] = None
+
+
+    @field_validator("action")
+    def validate_action(cls, v: str) -> str:
+        if v not in ["create", "get", "update", "delete"]:
+            raise ValueError(f"Invalid action: {v}")
+        return v
+    
+    @classmethod
+    def check_action(cls, action: str) -> None:
+        return cls.validate_action(action)
